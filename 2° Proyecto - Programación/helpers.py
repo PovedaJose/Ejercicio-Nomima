4836f0bab27c9a4f405d1e873dfaad6d700c3483
PovedaJose <85697518+PovedaJose@users.noreply.github.com>
2021-09-19 20:09:41 -0500

    Add files via upload

---
+++ 2° Proyecto - Programación/helpers.py
@@ -0,0 +1,13 @@
+import os
+
+def gotoxy(x,y): 
+    print("%c[%d;%df"%(0x1B,y,x),end="")
+    #& gotoxy(5,2): print('hola')
+
+def borrarPantalla():
+    os.system("cls") 
+
+def mensaje(msg,f,c):
+    pass
+
+
